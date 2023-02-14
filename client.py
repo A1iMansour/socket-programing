54f680c48785c9b65f642c55835092396dc6f3c0
A1iMansour <ali.mansour5531@outlook.com>
2023-02-14 21:25:55 +0200

    added distination server

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 ###                               MAC address????????????????????????????????????????????????????//
 
@@ -8,7 +9,21 @@ port=1999  #port number
 csocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)#TCP
 
 csocket.connect((host,port)) 
-request =input("type request: ")
 
+dest_address=input("what is the distination IP: ")
+
+""""GET /hello.txt HTTP/1.1
+User-Agent: curl/7.64.1
+Host: www.example.com
+Accept-Language: en, mi
+client_socket.sendall(("GET / HTTP/1.1\r\n\r\nDestination: " + destination_ip).encode())
+
+"""
+
+request =input("what is your request: ")
+print(f"request details:  | time: {time.ctime()}")
+exct_time=time.ctime()
+csocket.send(dest_address.encode('ascii'))
 csocket.send(request.encode('ascii'))
-print(csocket.recv(112).decode('ascii'))
+
+print(csocket.recv(112).decode('ascii')+f" | exact time:{exct_time}")
--- server.py
+++ server.py
@@ -17,7 +17,8 @@ while True:
 
     #incase error from client side
     try:
-        request=csocket.recv(112)
+        dest_address=csocket.recv(112)#taking address
+        request=csocket.recv(112)#taking request
     except ConnectionRefusedError:
         print("Message not recieved from client")
         csocket.send("there was problem with your message")
@@ -28,13 +29,13 @@ while True:
     dest_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:#any error occuring here will be from  server side
-        dest_socket.connect(("www.google.com", 80))
+        dest_socket.connect((dest_address.decode('ascii'), 80))#connect method will automatically translate string to its IP address
 
         #send the client's request to the destination server
-        dest_socket.sendall(request)
+        dest_socket.sendall(f"GET / HTTP/1.1\r\nHost:{dest_address}\r\n\r\n".encode('ascii'))
 
         #printing the message with exact time of the actual request
-        print(f"message recieved: {message}| actual time of request:{actual_time}".encode('ascii'))
+        print(f"request recieved: {message}| actual time of request:{actual_time}".encode('ascii'))
 
         #receive the response from the destination server
         response = dest_socket.recv(1024).decode('ascii')
@@ -43,7 +44,7 @@ while True:
          csocket.send("there was problem with the server,please try again")
 
     #printing a message that the response was received with the exact time
-    print(f"response was recieved from server: {response}|  time of response:{actual_time}".encode('ascii'))
+    print(f"response was recieved from server: {response}|  time of response:{time.ctime()}".encode('ascii'))
 
     #sending the client that response has been recieved
     csocket.send((f"respose {response}|  time of request:{time.ctime()}").encode('ascii'))
